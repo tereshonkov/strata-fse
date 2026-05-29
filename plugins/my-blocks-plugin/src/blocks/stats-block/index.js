4b67fad304d876ea7503a7c3c0b9bc1273b0662f
tereshonkov <tereshonkov.dima@gmail.com>
2026-05-29 16:32:41 +0300

    (fix) visual stules for admin gutenberg

--- plugins/my-blocks-plugin/src/blocks/stats-block/index.js
+++ plugins/my-blocks-plugin/src/blocks/stats-block/index.js
@@ -3,6 +3,7 @@ import Edit from './edit';
 import Save from './save';
 import metadata from './block.json';
 import './style.scss';
+import './editor.scss';
 
 registerBlockType( metadata.name, {
     edit: Edit,
